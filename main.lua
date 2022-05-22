c6ead8f07d5ce84fb80d78b98e6af0803cce8975
roko <40763333+rokoblox@users.noreply.github.com>
2022-05-22 21:38:34 +0200

    Update main.lua

--- main.lua
+++ main.lua
@@ -1,13 +1,16 @@
 local module = {}
 
-function module.Process(cmd, funcSet, plugin)
+function module.Process(cmd, funcSet, Plugin)
+    for i, v in pairs(Plugin) do
+        print(v)
+    end
     if cmd == "test1" then
         funcSet.println("test1 successful.")
     elseif cmd == "test2" then
         funcSet.println("test2 successful.")
     elseif cmd == "test3" then
-        plugin:SetSetting("#DEBUG", true)
-        println(tostring(plugin:GetSetting("#DEBUG")))
+        Plugin:SetSetting("#DEBUG", true)
+        println(tostring(Plugin:GetSetting("#DEBUG")))
     end
 end
 
